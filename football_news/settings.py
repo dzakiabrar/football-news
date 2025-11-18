9daf33766635e3da01ee9a0010428a0d1a9928c8
dzakiabrar <dzakiabrar.fi@gmail.com>
2025-11-19 01:27:04 +0700

    tutorial 8

--- football_news/settings.py
+++ football_news/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-ww-relvfxrtc)&5*uvr7_1p802vi+v!x+h!p!zmd6sp9b7uyft
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "dzaki-abrar-footballnews.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "dzaki-abrar-footballnews.pbp.cs.ui.ac.id",'10.0.2.2']
 CSRF_TRUSTED_ORIGINS = ["https://pbp.cs.ui.ac.id/web/project/dzaki.abrar/footballnews"]
 
 # Application definition
@@ -42,10 +42,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
+    'main',
+    'authentication',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -151,3 +154,11 @@ else:
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
+
